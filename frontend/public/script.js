dd806dee0b4366604eac7d1a9bfd3ed4226355c8
Takerazvan <108169843+Takerazvan@users.noreply.github.com>
2022-10-31 14:02:08 +0200

    [razvan], update

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,15 +1,17 @@
-let id = window.location.href.split('/').reverse()[0];
 
+let endpoint = window.location.href.split('/').reverse()[0];
+console.log(endpoint);
 let packageSchema = {
     pizza: [
         {
-            "id": "",
-            "name": "",
-            "ingredients": [],
-            "price": "",
-            "allergens": []
-        }]
-    }
+            id: '',
+            name: '',
+            ingredients: [],
+            price: '',
+            allergens: []
+        }
+    ]
+};
 function reInitPackageSchema() {
     packageSchema = {
         pizza: [
@@ -26,29 +28,31 @@ function reInitPackageSchema() {
 
 let pizzas;
 let allergens;
-    async function getData(str) {
-        const response = await fetch(`http://127.0.0.1:9000/api/${str}`);
 
-        const data = await response.json();
-        console.log(data);
+async function getData(str) {
+    const response = await fetch(`http://127.0.0.1:9000/api/${str}`);
 
-        if (str == "allergens") {
-            allergens = data.allergens
-        } else {
-             pizzas = data.pizza;
-        }
-        return data;
+    const data = await response.json();
+    console.log(data);
+
+    if (str == 'allergens') {
+        allergens = data.allergens;
+    } else {
+        pizzas = data.pizza;
+    }
+    return data;
 }
-    
 
 const loadEvent = async (_) => {
-    
-    const datapizza = await getData("pizza")
-    const dataAllergens = await getData("allergens")
-    console.log(datapizza)
-    console.log(dataAllergens)
-    
 
-}
+
+    const datapizza = await getData('pizza');
+    const dataAllergens = await getData('allergens');
+    console.log(datapizza);
+    console.log(dataAllergens);
+
+    packageSchema.pizza = datapizza;
+    console.log(packageSchema);
+};
 
 window.addEventListener('load', loadEvent);
