e10d2891a30e3c0def40d8e097cf83ef0422d4c1
Takerazvan <108169843+Takerazvan@users.noreply.github.com>
2022-11-03 12:29:07 +0200

    basket page info display

--- frontend/public/scriptForm.js
+++ frontend/public/scriptForm.js
@@ -1,25 +1,56 @@
 
 
 
-function addTocart(img, amount, total) {
+function addTocart(name, amount) {
     return `<div class="cartItems">
-    <img src="${img}" class="pizzaOrder"/>
-    
-    <button type="button" class="addItems">+</button>
-    ${amount}
-    <button type="button" class="removeItems">-</button>
-    <p>
-     Total price : ${total}
-    </p>
+     
+                        <div class="orders">
+                            <img
+                                width="30%"
+                                src="/public/images/pizza1.png"
+                                alt="pizza image"
+                            />
+
+                            <div><h2>
+                            ${name}  
+                            </h2>
+                                <h3></h3>
+                               
+                            </div>
+                            <div class="quantity">
+                                <i class="bi bi-patch-minus"></i>
+                                <div>${amount}</div>
+                                <i class="bi bi-patch-plus"></i>
+                            </div>
+                        </div>
     
     </div>`;
 }
 
-function cartItems(pizzas) {
+function cartItems(orders) {
+   
     return `<div class="AllPizzaContainer">
-        ${pizzas
+        ${orders[0].list
 
-            .map((elem) => addTocart(elem.name, elem.price, elem.image))
+            .map((elem) =>
+                addTocart(elem.name, elem.amount)
+            )
             .join('')}
     </div>`;
 }
+
+const loadEvent = async () => {
+    async function getData() {
+        return await (await fetch(`http://127.0.0.1:9000/pizza/orders`)).json();
+    }
+    const myOrders = await getData()
+    console.log(myOrders)
+    const rootElement = document.getElementById("root")
+
+    rootElement.insertAdjacentHTML(
+        'beforeend',
+        cartItems(myOrders)
+    );
+}
+
+window.addEventListener('load', loadEvent);
